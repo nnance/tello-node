11bc0734dd3e0e6003371efde1dd04e36d5f88a7
Nick Nance <nance.nick@gmail.com>
2019-08-02 08:39:58 -0700

    remove semicolons

--- src/listener.ts
+++ src/listener.ts
@@ -1,42 +1,42 @@
-import { createSocket, RemoteInfo, Socket } from "dgram";
-import { AddressInfo } from "net";
+import { createSocket, RemoteInfo, Socket } from "dgram"
+import { AddressInfo } from "net"
 
 export interface Logger {
-    (msg: string): void;
-};
+    (msg: string): void
+}
 
 export const errorHandler = (drone: Socket) => (err: Error | null, bytes: number) => {
-    if (err) console.log(`server error:\n${err.stack}`);
-    drone.close();
+    if (err) console.log(`server error:\n${err.stack}`)
+    drone.close()
 }
 
 const messageHandler = (msg: string, rinfo: RemoteInfo) => {
-    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`)
 }
 
 const listeningHandler = (drone: Socket) => () => {
-    const address = drone.address() as AddressInfo;
-    console.log(`server listening ${address.address}:${address.port}`);
+    const address = drone.address() as AddressInfo
+    console.log(`server listening ${address.address}:${address.port}`)
 }
 
 export const stateParser = (state: String) => {
-    const splitKeyVal = (key: string) => key.split(":");
-    const stateObj = (key: string) => ({ [splitKeyVal(key)[0]]: splitKeyVal(key)[1]});
-    const isKey = (val: string) => val.length > 0;
+    const splitKeyVal = (key: string) => key.split(":")
+    const stateObj = (key: string) => ({ [splitKeyVal(key)[0]]: splitKeyVal(key)[1]})
+    const isKey = (val: string) => val.length > 0
     const addKey = (acc: {}, key: string) => Object.assign(acc, stateObj(key))
 
-    const parsedObj = state.split(";").filter(isKey).reduce(addKey, {});
-    return parsedObj;
+    const parsedObj = state.split(";").filter(isKey).reduce(addKey, {})
+    return parsedObj
 }
 
-export const connect = (logger: Logger) => (port: number) => {
+export const connect = (logger: Logger) => (port: number, address?: string) => {
 
-    const drone = createSocket('udp4');
-    drone.bind(port);
+    const drone = createSocket('udp4')
+    drone.bind(port, address)
 
-    drone.on('error', errorHandler(drone));
-    drone.on('message', messageHandler);
-    drone.on('listening', listeningHandler(drone));
+    drone.on('error', errorHandler(drone))
+    drone.on('message', messageHandler)
+    drone.on('listening', listeningHandler(drone))
 
-    return drone;    
+    return drone    
 }
--- src/test/listener.ts
+++ src/test/listener.ts
@@ -1,6 +1,6 @@
-import { equal } from "assert";
+import { equal } from "assert"
 
-import { stateParser } from "../listener";
+import { stateParser } from "../listener"
 
 const droneStatus = "pitch:0;roll:0;yaw:0;vgx:0;vgy:0;vgz:0;templ:86;temph:88;tof:10;h:0;bat:19;baro:-24.59;time:0;agx:9.00;agy:-6.00;agz:-1000.00;"
 const droneStatusObj = {
@@ -25,8 +25,8 @@ const droneStatusObj = {
 describe('Drone Listener', () => {
     describe('stateParser', () => {
         it('should return the same value', () => {
-            const parsed = stateParser(droneStatus);
-            equal(Object.keys(parsed).length, Object.keys(droneStatusObj).length);
-        });
-    });
-});
+            const parsed = stateParser(droneStatus)
+            equal(Object.keys(parsed).length, Object.keys(droneStatusObj).length)
+        })
+    })
+})
