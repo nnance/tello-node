7525ba191b353b5021e7904abbddb7ba76917a9f
Nick Nance <nance.nick@gmail.com>
2019-08-04 19:17:02 -0700

    movement detected

--- src/sensors.ts
+++ src/sensors.ts
@@ -1,8 +1,36 @@
+export enum FlightState {
+    landed = 0,
+    moving,
+    hovering,
+}
+
+export interface FlightStateHandler {
+    (state: FlightState): void
+}
+
 const stateHandler = (queue: string[], msg: string) => {
     return queue.concat(msg)
 }
 
-export const eventProcessorFactory = (initialQueue = [] as string[]) => {
+const trimQueue = (maxQueueDepth: number) => (queue: string[]) => {
+    const start = queue.length - maxQueueDepth
+    return ([] as string[]).concat(queue.slice(start))
+}
+
+const isMoving = (queue: string[]) => {
+    return queue.filter((val) => val.indexOf("vgx:0;vgy:0;vgz:0;") < 0).length > 0
+}
+
+export const eventProcessorFactory = (cb: FlightStateHandler, maxQueueDepth = 10, initialQueue = [] as string[]) => {
     let queue = initialQueue
-    return (msg: string) => queue = stateHandler(queue, msg)
+    let trimmer = trimQueue(maxQueueDepth)
+    return (msg: string) => {
+        queue = trimmer(stateHandler(queue, msg))
+        if (isMoving(queue)) {
+            cb(FlightState.moving)
+        } else {
+            cb(FlightState.hovering)
+        }
+        return queue
+    }
 }
--- src/test/sensors.ts
+++ src/test/sensors.ts
@@ -2,14 +2,29 @@ import * as assert from "assert"
 
 import { describe } from "mocha"
 
-import { eventProcessorFactory } from "../sensors"
+import { eventProcessorFactory, FlightState } from "../sensors"
+
+const hoverQueue = [
+    "pitch:0;roll:0;yaw:0;vgx:0;vgy:0;vgz:0;templ:91;temph:94;tof:10;h:0;bat:89;baro:-26.40;time:17;agx:2.00;agy:-7.00;agz:-999.00;",
+    "pitch:0;roll:0;yaw:0;vgx:0;vgy:0;vgz:0;templ:91;temph:94;tof:10;h:0;bat:89;baro:-26.40;time:17;agx:2.00;agy:-7.00;agz:-999.00;",
+    "pitch:0;roll:0;yaw:0;vgx:0;vgy:0;vgz:0;templ:91;temph:94;tof:10;h:0;bat:89;baro:-26.40;time:17;agx:2.00;agy:-7.00;agz:-999.00;",
+    "pitch:0;roll:0;yaw:0;vgx:0;vgy:0;vgz:0;templ:91;temph:94;tof:10;h:0;bat:89;baro:-26.40;time:17;agx:2.00;agy:-7.00;agz:-999.00;",
+    "pitch:0;roll:0;yaw:0;vgx:0;vgy:0;vgz:0;templ:91;temph:94;tof:10;h:0;bat:89;baro:-26.40;time:17;agx:2.00;agy:-7.00;agz:-999.00;",
+    "pitch:0;roll:0;yaw:0;vgx:0;vgy:0;vgz:0;templ:91;temph:94;tof:10;h:0;bat:89;baro:-26.40;time:17;agx:2.00;agy:-7.00;agz:-999.00;",
+    "pitch:0;roll:0;yaw:0;vgx:0;vgy:0;vgz:0;templ:91;temph:94;tof:10;h:0;bat:89;baro:-26.40;time:17;agx:2.00;agy:-7.00;agz:-999.00;",
+    "pitch:0;roll:0;yaw:0;vgx:0;vgy:0;vgz:0;templ:91;temph:94;tof:10;h:0;bat:89;baro:-26.40;time:17;agx:2.00;agy:-7.00;agz:-999.00;",
+    "pitch:0;roll:0;yaw:0;vgx:0;vgy:0;vgz:0;templ:91;temph:94;tof:10;h:0;bat:89;baro:-26.40;time:17;agx:2.00;agy:-7.00;agz:-999.00;",
+    "pitch:0;roll:0;yaw:0;vgx:0;vgy:0;vgz:0;templ:91;temph:94;tof:10;h:0;bat:89;baro:-26.40;time:17;agx:2.00;agy:-7.00;agz:-999.00;",
+]
+
+const voidHandler = (state: FlightState) => undefined
 
 describe("Movement Sensor", () => {
     describe("when receiving an event", () => {
         const initialQueue = [""]
         let queue: string[]
         before(() => {
-            const handler = eventProcessorFactory(initialQueue)
+            const handler = eventProcessorFactory(voidHandler, 10, initialQueue)
             queue = handler("")
         })
         it("should return a new array", () => {
@@ -17,6 +32,32 @@ describe("Movement Sensor", () => {
         })
         it("should have two elements", () => {
             assert.equal(queue.length, 2)
+        })
+    })
+
+    describe("when receiving more events than the queue depth", () => {
+        let queue: string[]
+        before(() => {
+            const handler = eventProcessorFactory(voidHandler, 10, hoverQueue)
+            queue = handler("")
+        })
+        it("should return an array with max queue depth size", () => {
+            assert.equal(queue.length, 10)
+        })
+    })
+
+    describe("when receiving events with movement", () => {
+        let state: FlightState
+        const stateHandler = (val: FlightState) => state = val
+        const handler = eventProcessorFactory(stateHandler, 10, hoverQueue)
+
+        it("should return moving state", () => {
+            handler("pitch:0;roll:0;yaw:0;vgx:0;vgy:0;vgz:-1;templ:91;temph:94;tof:10;h:0;bat:89;baro:-26.51;time:18;agx:-12.00;agy:-4.00;agz:-1245.00;")
+            assert.equal(state, FlightState.moving)
+        })
+        it("should return hovering state", () => {
+            hoverQueue.forEach((event) => handler(event))
+            assert.equal(state, FlightState.hovering)
         })
     })
 })
